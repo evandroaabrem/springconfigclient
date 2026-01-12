2fb0a34b90e270618bae72b0868833b114c21d91
Evandro Abreu <evandro_araujo@yahoo.com.br>
2026-01-12 09:05:02 -0300

    start

---
+++ src/main/java/com/example/SpringConfigClientApplication.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class SpringConfigClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringConfigClientApplication.class, args);
+    }
+
+    @RestController
+    static class TestController {
+        @Value("${sample.message:mensagem-default}")
+        private String message;
+
+        @GetMapping("/message")
+        public String message() {
+            return message;
+        }
+    }
+}
+
